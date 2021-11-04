bb0157d94de6f28311244714e7a9c37f654d543f
shanchu815 <shanchu815@gmail.com>
2021-11-04 19:53:13 +0000

    list_operations functions fixed

---
+++ list-slicing/list_operations.py
@@ -0,0 +1,239 @@
+"""Utilities for manipulating lists."""
+
+
+def head(input_list):
+    """Return the first item of the input list.
+
+    For example:
+
+      >>> head(['Jan', 'Feb', 'Mar'])
+      'Jan'
+    """
+
+    return input_list[0]
+
+    #return None
+
+
+def tail(input_list):
+    """Return a new list of all items, excluding the first item.
+
+    For example:
+
+    >>> tail(['Jan', 'Feb', 'Mar'])
+    ['Feb', 'Mar']
+
+    """
+
+    return input_list[1:]
+
+
+def last(input_list):
+    """Return the last item of the input list.
+
+    For example:
+
+    >>> last(['Jan', 'Feb', 'Mar'])
+    'Mar'
+
+    """
+
+    return input_list[-1]
+
+
+def top(input_list):
+    """Return all elements of the input list except the last.
+
+    For example:
+
+    >>> top(['Jan', 'Feb', 'Mar'])
+    ['Jan', 'Feb']
+
+    """
+
+    return input_list[0:-1]
+
+
+def first_three(input_list):
+    """Return the first three elements of the input list.
+
+    For example:
+
+    >>> first_three(['Jan', 'Feb', 'Mar', 'Apr', 'May'])
+    ['Jan', 'Feb', 'Mar']
+
+    """
+
+    return input_list[0:3]
+
+
+def last_five(input_list):
+    """Return the last five elements of the input list.
+
+    For example:
+
+    >>> last_five([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [15, 18, 21, 24, 27]
+
+    """
+
+    return input_list[-5:]
+
+
+def middle(input_list):
+    """Return all elements of input_list except the first two and the last two.
+
+    For example:
+
+    >>> middle([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [6, 9, 12, 15, 18, 21]
+
+    """
+    return input_list[2:-2]
+
+#[0:0:0]
+#starts at 2
+#end would be -2
+#[2:-2]
+
+'''colors = ['red', 'green', 'blue', 'yellow', 'orange']
+
+# List containing only last 2 = ['yellow', 'orange']
+print(colors[-2:])
+
+# List excluding last 2 = ['red', 'green', 'blue']
+print(colors[:-2])
+'''
+
+def inner_four(input_list):
+    """Return the third, fourth, fifth, and sixth elements of input_list.
+
+    For example:
+
+    >>> inner_four([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [6, 9, 12, 15]
+
+    """
+
+    return input_list[2:6]
+
+
+def inner_four_end(input_list):
+    """Return the elements that are 6th, 5th, 4th, and 3rd from the end of input_list.
+
+    This function should return those elements in a list, in the exact order
+    described above.
+
+    For example:
+
+    >>> inner_four_end([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [12, 15, 18, 21]
+
+    """
+
+    return input_list[-6:-2]
+
+
+def replace_head(input_list):
+    """Replace the head of input_list with the value 42 and return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_head(multiples)
+    >>> multiples == [42, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    True
+
+    """
+    input_list[0] = 42
+    # pass
+    return None
+
+def replace_third_and_last(input_list):
+    """Replace third and last elements of input_list with 37 and return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_third_and_last(multiples)
+    >>> multiples == [0, 3, 37, 9, 12, 15, 18, 21, 24, 37]
+    True
+
+    """
+    input_list[-1] = 37
+    input_list[2] = 37
+    # pass
+    return None
+
+
+def replace_middle(input_list):
+    """Replace all elements of a list but the first two and last two with 42 and 37.
+
+    After the replacement, 42 and 37 should appear in that order in input_list.
+
+    Return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_middle(multiples)
+    >>> multiples == [0, 3, 42, 37, 24, 27]
+    True
+
+    """
+    input_list[2:-2] = [42,37]
+    # pass
+    return None
+
+'''aList = [1,2,3,4,5]
+#minus 1 because the first element is index 0
+middleIndex = (len(aList) - 1)/2'''
+
+
+def delete_third_and_seventh(input_list):
+    """Remove third and seventh elements of input_list and return nothing.
+
+    For example:
+
+    >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+    >>> delete_third_and_seventh(notes)
+    >>> notes == ['Do', 'Re', 'Fa', 'So', 'La', 'Do']
+    True
+
+    """
+    input_list.pop(2)
+    input_list.pop(5)
+    # pass
+    return None
+
+
+def delete_middle(input_list):
+    """Remove all elements from input_list except the first two and last two.
+
+    Return nothing.
+
+    For example:
+
+    >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+    >>> delete_middle(notes)
+    >>> notes == ['Do', 'Re', 'Ti', 'Do']
+    True
+
+    """
+    del input_list[2:-2]
+    # pass
+    return None
+
+'''numbers = [50, 60, 70, 80]
+del numbers[1:2]
+print(numbers) # [50, 70, 80]'''
+
+
+# This is the part were we actually run the doctests.
+
+if __name__ == "__main__":
+    import doctest
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("ALL TESTS PASSED")
